50edde3a65344db7ab2d16ee2440a95b1bfd11a3
Akira Yanagisawa <ayanax0@s8.dion.ne.jp>
2018-09-13 13:58:22 +0900

    commit

--- Office365/ShowLicense.ps1
+++ Office365/ShowLicense.ps1
@@ -3,10 +3,16 @@
 # Install-Module -Name MSOnline
 
 # ユーザー認証情報取得
-$userCredential = Get-Credential -UserName "akiray@interestec.onmicrosoft.com" -Message "Enter Password."
+#$userCredential = Get-Credential -Credential "akiray@interestec.onmicrosoft.com"
+#$userCredential = Get-Credential -UserName "akiray@scskwin.com" -Message "Enter Password."
+$userCredential = Get-Credential
 
 # Office365に接続
 Connect-MsolService -Credential $userCredential
 
 # ログインユーザーのライセンスを表示
 Get-MsolAccountSku
+
+# ユーザー情報の表示
+Get-MsolUser | ForEach-Object { Write-Output $_.DisplayName }
+#Get-MsolUser -All
